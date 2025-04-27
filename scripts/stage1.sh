116906b26bd81905f3931c80e890e0df293e2e09
bodashka <bodashkaxdgg@gmail.com>
2025-04-27 17:23:35 +0300

    Stage 1 completed

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+ROOT_DIR=$(realpath "$SCRIPT_DIR/..")
+
+echo ">> Creating venv..."
+bash "$SCRIPT_DIR/1_0_create_venv.sh"
+
+echo ">> Downloading data..."
+bash "$SCRIPT_DIR/1_1_download_data.sh"
+
+echo ">> Running pipeline..."
+bash "$SCRIPT_DIR/1_5_run_pipeline.sh"
